1023138e7c60102f7ab6e0938517816f216b85f8
oscarbfs <oscar.borgesfs@gmail.com>
2022-06-06 21:36:14 -0300

    Implementando Ticket

--- LPOO/desafio_1/Ticket.java
+++ LPOO/desafio_1/Ticket.java
@@ -1,3 +1,20 @@
 public class Ticket {
-    
+    private Cliente cliente;
+    private Veiculo veiculo;
+    public Ticket(Cliente cliente, Veiculo veiculo) {
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
 }
